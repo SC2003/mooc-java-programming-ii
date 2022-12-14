f1dc3ad69406fcad4918b80908e3175464479d1c
SC2003 <46285019+SC2003@users.noreply.github.com>
2022-12-14 16:28:28 +0530

    Part 13

---
+++ Part-13/part13-Part13_08.UserTitle/src/main/java/title/Main.java
@@ -0,0 +1,18 @@
+package title;
+
+import java.util.Scanner;
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter the Title: ");
+        String title = scanner.nextLine();
+        
+        Application.launch(UserTitle.class, "--title="+title);
+
+    }
+
+}
